de330f821f5dace204dfa04db5599d7e6fb82260
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2023-12-13 18:50:37 +0100

    improved parameters' names and made methods more concise

--- src/a02b/e1/UniversityProgramFactoryImpl.java
+++ src/a02b/e1/UniversityProgramFactoryImpl.java
@@ -1,9 +1,6 @@
 package a02b.e1;
 
-import static a02b.e1.UniversityProgram.Sector.COMPUTER_ENGINEERING;
-
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -53,16 +50,16 @@ public class UniversityProgramFactoryImpl implements UniversityProgramFactory {
         };
     }
 
-    private UniversityProgram generalCaller(Set<Set<Sector>> sectors, Map<Set<Sector>, Pair<Integer, ComparationMethod>> comparation){
-        return general(sectors, pair -> {
-            Set<Sector> sectorSet = pair.get1();
-            Integer sum = pair.get2();
-            Integer expectedSum = comparation.get(sectorSet).get1();
-            ComparationMethod comparationMethod = comparation.get(sectorSet).get2();
+    private UniversityProgram generalCaller(Set<Set<Sector>> allSectors, Map<Set<Sector>, Pair<Integer, ComparationMethod>> expected){
+        return general(allSectors, actual -> {
+            Set<Sector> sectors = actual.get1();
+            Integer actualSum = actual.get2();
+            Integer expectedSum = expected.get(sectors).get1();
+            ComparationMethod comparationMethod = expected.get(sectors).get2();
             switch (comparationMethod) {
-                case EQUAL: return sum == expectedSum;
-                case GREATER_OR_EQUAL: return sum >= expectedSum;
-                case LESS_OR_EQUAL: return sum <= expectedSum;
+                case EQUAL: return actualSum == expectedSum;
+                case GREATER_OR_EQUAL: return actualSum >= expectedSum;
+                case LESS_OR_EQUAL: return actualSum <= expectedSum;
                 default: throw new IllegalStateException();
             }
         });
@@ -70,62 +67,40 @@ public class UniversityProgramFactoryImpl implements UniversityProgramFactory {
 
     @Override
     public UniversityProgram flexible() {
-        Set<Set<Sector>> sectors = new HashSet<>();
-        Set<Sector> all = Set.of(Sector.values());
-        sectors.add(all);
-        Map<Set<Sector>, Pair<Integer, ComparationMethod>> sectorsToExpectedSums = new HashMap<>();
-        sectorsToExpectedSums.put(all, new Pair<>(60, ComparationMethod.EQUAL));
-        return generalCaller(sectors, sectorsToExpectedSums);
+        Map<Set<Sector>, Pair<Integer, ComparationMethod>> expected = new HashMap<>();
+        expected.put(Set.of(Sector.values()), new Pair<>(60, ComparationMethod.EQUAL));
+        Set<Set<Sector>> sectors = expected.keySet();
+        return generalCaller(sectors, expected);
     }
 
     @Override
     public UniversityProgram scientific() {
-        Set<Set<Sector>> sectors = new HashSet<>();
-        Set<Sector> all = Set.of(Sector.values());
-        Set<Sector> compSci = Set.of(Sector.COMPUTER_SCIENCE);
-        Set<Sector> physics = Set.of(Sector.PHYSICS);
-        Set<Sector> math = Set.of(Sector.MATHEMATICS);
-        sectors.add(all);
-        sectors.add(compSci);
-        sectors.add(physics);
-        sectors.add(math);
-        Map<Set<Sector>, Pair<Integer, ComparationMethod>> sectorsToExpectedSums = new HashMap<>();
-        sectorsToExpectedSums.put(all, new Pair<>(60, ComparationMethod.EQUAL));
-        sectorsToExpectedSums.put(compSci, new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
-        sectorsToExpectedSums.put(physics, new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
-        sectorsToExpectedSums.put(math, new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
-        return generalCaller(sectors, sectorsToExpectedSums);
+        Map<Set<Sector>, Pair<Integer, ComparationMethod>> expected = new HashMap<>();
+        expected.put(Set.of(Sector.values()), new Pair<>(60, ComparationMethod.EQUAL));
+        expected.put(Set.of(Sector.COMPUTER_SCIENCE), new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
+        expected.put(Set.of(Sector.PHYSICS), new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
+        expected.put(Set.of(Sector.MATHEMATICS), new Pair<>(12, ComparationMethod.GREATER_OR_EQUAL));
+        Set<Set<Sector>> sectors = expected.keySet();
+        return generalCaller(sectors, expected);
     }
 
     @Override
     public UniversityProgram shortComputerScience() {
-        Set<Set<Sector>> sectors = new HashSet<>();
-        Set<Sector> all = Set.of(Sector.values());
-        Set<Sector> compSci = Set.of(Sector.COMPUTER_ENGINEERING, Sector.COMPUTER_SCIENCE);
-        sectors.add(all);
-        sectors.add(compSci);
-        Map<Set<Sector>, Pair<Integer, ComparationMethod>> sectorsToExpectedSums = new HashMap<>();
-        sectorsToExpectedSums.put(all, new Pair<>(48, ComparationMethod.GREATER_OR_EQUAL));
-        sectorsToExpectedSums.put(compSci, new Pair<>(30, ComparationMethod.GREATER_OR_EQUAL));
-        return generalCaller(sectors, sectorsToExpectedSums);
+        Map<Set<Sector>, Pair<Integer, ComparationMethod>> expected = new HashMap<>();
+        expected.put(Set.of(Sector.values()), new Pair<>(48, ComparationMethod.GREATER_OR_EQUAL));
+        expected.put(Set.of(Sector.COMPUTER_ENGINEERING, Sector.COMPUTER_SCIENCE), new Pair<>(30, ComparationMethod.GREATER_OR_EQUAL));
+        Set<Set<Sector>> sectors = expected.keySet();
+        return generalCaller(sectors, expected);
     }
 
     @Override
     public UniversityProgram realistic() {
-        Set<Set<Sector>> sectors = new HashSet<>();
-        Set<Sector> all = Set.of(Sector.values());
-        Set<Sector> compSci = Set.of(Sector.COMPUTER_ENGINEERING, Sector.COMPUTER_SCIENCE);
-        Set<Sector> mathPhys = Set.of(Sector.MATHEMATICS, Sector.PHYSICS);
-        Set<Sector> thesis = Set.of(Sector.THESIS);
-        sectors.add(all);
-        sectors.add(compSci);
-        sectors.add(mathPhys);
-        sectors.add(thesis);
-        Map<Set<Sector>, Pair<Integer, ComparationMethod>> sectorsToExpectedSums = new HashMap<>();
-        sectorsToExpectedSums.put(all, new Pair<>(120, ComparationMethod.EQUAL));
-        sectorsToExpectedSums.put(compSci, new Pair<>(60, ComparationMethod.GREATER_OR_EQUAL));
-        sectorsToExpectedSums.put(mathPhys, new Pair<>(18, ComparationMethod.LESS_OR_EQUAL));
-        sectorsToExpectedSums.put(thesis, new Pair<>(24, ComparationMethod.EQUAL));
-        return generalCaller(sectors, sectorsToExpectedSums);
+        Map<Set<Sector>, Pair<Integer, ComparationMethod>> expected = new HashMap<>();
+        expected.put(Set.of(Sector.values()), new Pair<>(120, ComparationMethod.EQUAL));
+        expected.put(Set.of(Sector.COMPUTER_ENGINEERING, Sector.COMPUTER_SCIENCE), new Pair<>(60, ComparationMethod.GREATER_OR_EQUAL));
+        expected.put(Set.of(Sector.MATHEMATICS, Sector.PHYSICS), new Pair<>(18, ComparationMethod.LESS_OR_EQUAL));
+        expected.put(Set.of(Sector.THESIS), new Pair<>(24, ComparationMethod.EQUAL));
+        Set<Set<Sector>> sectors = expected.keySet();
+        return generalCaller(sectors, expected);
     }
 }
